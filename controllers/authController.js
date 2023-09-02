6f938e1297da65b963859664ad4252099e3d57d9
anwarkvzk <anvarvzk521@gmail.com>
2023-09-02 23:34:49 +0530

    Login API with Postman created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const userModels = require("../models/userModels");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerController = async (req, res) => {
   try {
@@ -32,7 +33,39 @@ const registerController = async (req, res) => {
   }
 };
 //Login CallBack
-const loginController = () => {
-    
-}
+const loginController = async (req, res) => {
+  try {
+    const user = await userModels.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(404).send({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+    const comparePassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if(!comparePassword){
+      return res.status(500).send({
+        success: false,
+        message: 'Invalid Credentials'
+      })
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {expiresIn: '1d'});
+    return res.status(200).send({
+      success:true,
+      message: 'Login Successfully',
+      token ,
+      user, 
+    })
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error In Login API",
+      error,
+    });
+  }
+};
 module.exports = { registerController, loginController };
