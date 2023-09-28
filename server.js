e4e64f797da7017a1d1477f97bd039a14203264e
anwarkvzk <anvarvzk521@gmail.com>
2023-09-28 08:57:10 +0530

    Static File and Folders Are added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 //express
 const app = express();
@@ -25,6 +26,14 @@ app.use("/api/v1/auth", require("./routes/authRoutes"));
 app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/AdminRoutes"));
+
+//static file
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+//Static Routes
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
